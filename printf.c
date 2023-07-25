789a27e5a3b7f774d878ce2f976c6f9310de14f5
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-07-25 08:43:12 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- printf.c
+++ printf.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 /**
  * _printf - produces output according to a format
- * @format: format string 
+ * @format: format string
  * Description: this function will call the get_print() function that will
  *
  * Return: length of the formatted output string
@@ -14,7 +15,7 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-
+	
 	va_start(args, format);
 	while (*format != '\0')
 	{
@@ -24,10 +25,10 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					count += write(1, &va_arg(args, int),1);
+					count += write(1, &args, 1);
 					break;
 				case 's':
-					count += prrintf("%s", va_arg(args, char*));
+					count += printf("%s", va_arg(args, char*));
 					break;
 				case '%':
 					count += putchar('%');
@@ -40,7 +41,7 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			putchar(*formar);
+			putchar(*format);
 			count++;
 		}
 		format++;
